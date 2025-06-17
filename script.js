e3ba5253373dbce91a09d7ea902dac0fe1166682
drei <delosreyesandrei97@gmail.com>
2025-06-18 05:25:41 +0800

    Deleted the script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-
-console.log("Hello my name is Drei");
-console.log("Im from matabungkay,lian,batangas");
-console.log("Im 21 years old");
-console.log("In an active person in physical activities");
